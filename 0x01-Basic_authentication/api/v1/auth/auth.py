f0595c5d23cfe735f110bdb7ad27e4814519e2da
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-11-24 21:40:43 +0000

    require auth public method implemented

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,14 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns False
         """
-        return False
+        paths = path
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != "/":
+            paths += "/"
+        if paths in excluded_paths or path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """returns None
