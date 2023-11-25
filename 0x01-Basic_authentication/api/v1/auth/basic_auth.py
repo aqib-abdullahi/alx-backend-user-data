cae3f212063a94de6e5558a79ffbdc7fd4875899
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-11-25 09:12:24 +0000

    Added BasicAuth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Basic authentication
+module
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic auth class inheriting from
+    auth class
+    """
+    pass
