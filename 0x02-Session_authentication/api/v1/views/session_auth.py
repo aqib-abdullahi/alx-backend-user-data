6d3e8950d9f99fa5d53ed21e7d6be152930ff718
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-11-26 21:55:25 +0000

    session authentication views added

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Session authentication
+"""
+from flask import request, jsonify, abort
+from api.v1.views import app_views
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """ login session authentication
+    """
+    user_email = request.form.get('email')
+    user_password = request.form.get('password')
+    if not user_email:
+        return jsonify({"error": "email missing"}), 400
+    if not user_password:
+        return jsonify({"error": "password missing"}), 400
+    try:
+        user = User.search({"email": user_email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    for u in user:
+        if u.is_valid_password(user_password):
+            user_id = u.id
+            from api.v1.app import auth
+            session_id = auth.create_session(user_id)
+            response = jsonify(u.to_json())
+            response.set_cookie(getenv('SESSION_NAME'), session_id)
+            return response
+        else:
+            return jsonify({"error": "wrong password"}), 401
+    return jsonify({"error": "no user found for this emai"}), 404
